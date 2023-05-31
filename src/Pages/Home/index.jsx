9f9af05aa322bf962214940dada6b8f81c4e0434
schweigenderFlugel <facundoezcastro@gmail.com>
2023-05-31 02:17:24 -0300

    Consumiendo API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,24 @@
+import React from 'react';
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
 
 function Home() {
-    return (
-        <Layout>
-            Home
-            <Card/>
-        </Layout>
-    )
-  }
+  const [ items, setItems ] = React.useState(null)
+  React.useEffect(() => {
+    // Con fetch estamos diciendo que el Home necesita de la infomación de la API. 
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
+
+  return (
+    <Layout>
+      Home
+        <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+          { items?.map((item) => ( <Card key={item.id} data={item} />) )}
+        </div>
+    </Layout>
+  )
+}
   
-  export default Home
+export default Home
