954e6433a04b5a59b6063e6e9681948cb508efa2
schweigenderFlugel <facundoezcastro@gmail.com>
2023-06-10 14:06:15 -0300

    Modificaciones a la pantalla de login

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './Pages/App/index.jsx'
-import { Auth0Provider } from '@auth0/auth0-react'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./Pages/App/index.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
+import "./index.css";
 
-const domain = import.meta.env.VITE_APP_DOMAIN;
-const clientId = import.meta.env.VITE_CLIENT_ID;
-console.log(domain);
-console.log(clientId);
+const domain = import.meta.env.VITE_AUTH0_APP_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+// const apiAudience = import.meta.env.VITE_AUTH0_API_AUDIENCE
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Auth0Provider 
-          domain={domain} 
-          clientId={clientId}
-          authorizationParams={{
-          redirect_uri: window.location.origin,
-          }}
-        >
-           <App />
-        </Auth0Provider>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
-)
+);
